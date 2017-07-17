6731369facc9ff2f58505ede6e21299cd44c2d94
Guochun.Gong <yqsy021@126.com>
2017-07-17 13:21:37 +0800

    staticmethod 以及定一个模型

--- analyze_dir.py
+++ analyze_dir.py
@@ -25,36 +25,11 @@ parser.add_option('-v', '--verbose', dest='verbose', action="store_true",
                   help='verbose')
 
 
-def need_judge_text(file):
-    """根据后缀名判断文件是否需要检查"""
-    head, tail = os.path.split(file)
-    _, extension = os.path.splitext(tail)
-
-    include_extensions = ['.py', '.html', '.js', '.css', '.conf',
-                          '.txt', '.sh', '.pas', '.sql', '.sqc',
-                          '.h', '.cpp', '.ini', '.H', '.txt',
-                          '.sql', '.CPP', '.rb']
-
-    if extension in include_extensions:
-        return True
-
-    return False
-
-
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
 class FileAttr():
     def __init__(self, suffix, files):
         self.suffix = suffix
         self.files = files
         self.file_numbers = len(files)
-        self.mimes = {}
         self.newlines = {}
         self.encodings = {}
         self.size = 0
@@ -69,7 +44,7 @@ class FileAttr():
                                       self.file_numbers,
                                       self.newlines,
                                       self.encodings,
-                                      sizeof_fmt(self.size),
+                                      self.sizeof_fmt(self.size),
                                       self.lines,
                                       self.tab_or_space)
         else:
@@ -77,6 +52,36 @@ class FileAttr():
 
     def __lt__(self, other):
         return self.file_numbers < other.file_numbers
+
+    @staticmethod
+    def need_judge_text(file):
+        """根据后缀名判断文件是否需要检查"""
+        head, tail = os.path.split(file)
+        _, extension = os.path.splitext(tail)
+
+        include_extensions = ['.py', '.html', '.js', '.css', '.conf',
+                              '.txt', '.sh', '.pas', '.sql', '.sqc',
+                              '.h', '.cpp', '.ini', '.H', '.txt',
+                              '.sql', '.CPP', '.rb']
+
+        if extension in include_extensions:
+            return True
+
+        return False
+
+    @staticmethod
+    def sizeof_fmt(num, suffix='B'):
+        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Yi', suffix)
+
+    def inspect_file(self):
+        """检查所有文件属性
+        1.encoding 2.newline 3.size 4.line 5.tab or space
+        """
+        
 
 
 def get_filter_suffixs():
@@ -165,65 +170,5 @@ if __name__ == '__main__':
     file_attrs = get_file_attrs(extension_dict)
 
     for file_attr in file_attrs:
-        for file in file_attr.files:
-
-            need_judge = need_judge_text(file)
-
-            # encoding
-            encoding = None
-
-            file_attr.need_judge = need_judge
-
-            if need_judge:
-                with open(file, 'rb') as f:
-                    encoding = chardet.detect(f.read())['encoding']
-                    file_attr.encodings[encoding] = file_attr.encodings.get(
-                        encoding, 0) + 1
-            else:
-                file_attr.encodings['?'] = file_attr.encodings.get('?', 0) + 1
-
-            # newline
-            if need_judge:
-                with open(file, 'rb') as f:
-                    line = f.readline()
-
-
-                    def get_new_line(line):
-                        new_line_characters = [b'\r\n', b'\r', b'\n', b'']
-                        for c in new_line_characters:
-                            if line.endswith(c):
-                                return c.decode()
-                        return None
-
-
-                    c = get_new_line(line)
-                    if c != None:
-                        file_attr.newlines[c] = file_attr.newlines.get(
-                            c, 0) + 1
-                    else:
-                        file_attr.newlines['?'] = file_attr.newlines.get(
-                            '?', 0) + 1
-
-            else:
-                file_attr.newlines['?'] = file_attr.newlines.get('?', 0) + 1
-
-            # size
-            file_attr.size = file_attr.size + os.path.getsize(file)
-
-            # line
-            if need_judge:
-                file_attr.lines = file_attr.lines + \
-                                  sum(1 for line in open(file, 'rb'))
-
-            # tab or space
-            if need_judge:
-                with open(file, 'rb') as f:
-                    if f.read(500).find(b'\t') != -1:
-                        file_attr.tab_or_space['\t'] = file_attr.tab_or_space.get(
-                            '\t', 0) + 1
-                    else:
-                        file_attr.tab_or_space[r'space'] = file_attr.tab_or_space.get(
-                            r'space', 0) + 1
-
-
+        file_attr.inspect_file()
         print(file_attr)
